c4091032b74822b06e0d7cafa72c8df91a64c1a9
Sergio Javier Vergara Ganado <sevega69@msn.com>
2020-02-07 23:24:09 -0600

    Agregar archivos de proyecto.

---
+++ prLocalizaOrdenesFraudolentasV2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace prLocalizaOrdenesFraudolentasV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] ordenesId = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+            foreach (string ordenId in ordenesId)
+            {
+                if (ordenId.StartsWith("B"))
+                {
+                    Console.WriteLine($"Ordenes que inician con B {ordenId}");
+                }
+            }
+        }
+    }
+}
